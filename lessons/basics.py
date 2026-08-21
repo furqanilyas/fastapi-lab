3e1507dfbd6f74923d704be9d77c0e87885d103f
Furqan Ilyas <furqanilyas2005@gmail.com>
2026-08-21 18:52:27 +0500

    Add products for testing

--- lessons/basics.py
+++ lessons/basics.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from models import Product
 app = FastAPI()
 
 @app.get("/")
 def greet():
     return "Hello World"
 
+product1 = Product(name = "Laptop", id=1, description="Gaming laptop",
+                   price=100, quantity=5)
+product2 = Product(name = "Mobile", id=2, description="Usage Mobiles",
+                   price=50, quantity=10)
 @app.get("/products")
 def products():
-    return "Gimme all products"
+    return product1, product2
